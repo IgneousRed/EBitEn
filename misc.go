e3c65d3aa221cc357e7b98c579899f097b69bd94
IgneousRed <IgneousRed@protonmail.com>
2022-10-20 22:08:36 +0200

    a

--- misc.go
+++ misc.go
@@ -1,6 +1,10 @@
 package ebitengine
 
-import "image/color"
+import (
+	"image/color"
+
+	m "github.com/IgneousRed/gomisc"
+)
 
 var Black = color.RGBA{0, 0, 0, 255}
 var White = color.RGBA{255, 255, 255, 255}
@@ -10,3 +14,7 @@ var Blue = color.RGBA{0, 0, 255, 255}
 var Yellow = color.RGBA{255, 255, 0, 255}
 var Cyan = color.RGBA{0, 255, 255, 255}
 var Magenta = color.RGBA{255, 0, 255, 255}
+
+func Vec2(x, y float32) m.Vec[float32] {
+	return m.Vec[float32]{x, y}
+}
