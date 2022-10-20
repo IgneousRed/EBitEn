03a36eaf19e47ad22167aa459590f3631fa3cb09
IgneousRed <IgneousRed@protonmail.com>
2022-10-20 22:00:26 +0200

    a

--- text.go
+++ text.go
@@ -7,27 +7,37 @@ import (
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
 
+	m "github.com/IgneousRed/gomisc"
 	eb "github.com/hajimehoshi/ebiten/v2"
-	// text "github.com/hajimehoshi/ebiten/v2/text"
+	text "github.com/hajimehoshi/ebiten/v2/text"
 )
+
+type Font struct {
+	face font.Face
+	size float32
+}
 
 var emptyImg = eb.NewImage(1, 1)
 
 func init() {
 	emptyImg.Fill(color.White)
 }
-func Font(path string, size float64) (font.Face, error) {
-	var result font.Face
+func NewFont(path string, size float32) (Font, error) {
+	var result Font
 	if fcm, err := os.ReadFile(path); err != nil {
 		return result, err
 	} else if ttf, err := opentype.Parse(fcm); err != nil {
 		return result, err
-	} else if result, err = opentype.NewFace(ttf, &opentype.FaceOptions{
-		Size:    size,
+	} else if face, err := opentype.NewFace(ttf, &opentype.FaceOptions{
+		Size:    float64(size),
 		DPI:     72,
 		Hinting: font.HintingFull,
 	}); err != nil {
 		return result, err
+	} else {
+		return Font{face, size}, nil
 	}
-	return result, nil
+}
+func DrawText(scr *eb.Image, font Font, txt string, pos m.Vec[float32], clr color.Color) {
+	text.Draw(scr, txt, font.face, int(pos[0]), int(pos[1]+font.size), clr)
 }
