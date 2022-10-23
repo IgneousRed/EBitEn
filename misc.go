1c0917499c0092dc28eb67ce8013b772f0075a17
IgneousRed <IgneousRed@protonmail.com>
2022-10-23 22:50:33 +0200

    a

--- misc.go
+++ misc.go
@@ -15,6 +15,9 @@ var Yellow = color.RGBA{255, 255, 0, 255}
 var Cyan = color.RGBA{0, 255, 255, 255}
 var Magenta = color.RGBA{255, 0, 255, 255}
 
-func Vec2(x, y float32) m.Vec[float32] {
+func Vec2i(x, y int) m.Vec[int] {
+	return m.Vec[int]{x, y}
+}
+func Vec2f(x, y float32) m.Vec[float32] {
 	return m.Vec[float32]{x, y}
 }
